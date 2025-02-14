dbfc13aa5d140dbb74590b044eb827093b44a678
andrew-dot-exe <andrejnikitin974@gmail.com>
2025-02-15 00:40:52 +0400

    gui almost ready

--- app/src/main/java/com/andrewexe/ui/MainWindow.java
+++ app/src/main/java/com/andrewexe/ui/MainWindow.java
@@ -1,11 +1,16 @@
 package com.andrewexe.ui;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
 
-public class MainWindow {
+public class MainWindow{
 
     private JFrame mainFrame;
     private JMenuBar mJMenuBar;
+
+    private JScrollPane areaScrollPane;
+    private JTextArea textArea;
 
     private JFrame getJFrame() {
         if (mainFrame == null) {
@@ -20,17 +25,73 @@ public class MainWindow {
             mJMenuBar = new JMenuBar();
             // fill 
             JMenu menu = new JMenu("File");
+            JMenuItem openItem = new JMenuItem("Open");
+            
+            JMenuItem saveItem = new JMenuItem("Save");
+
+            JMenuItem closeProgram = new JMenuItem("Close");
+            closeProgram.addActionListener(new ExitMenuItemHandler());
+
+            menu.add(openItem);
+            menu.add(saveItem);
+            menu.add(closeProgram);
+
             mJMenuBar.add(menu);
         }
         return mJMenuBar;
     }
+
+    public JTextArea getTextArea(){
+        if(textArea == null){
+            textArea = new JTextArea( );
+            textArea.setLineWrap(true);
+        }
+        return textArea;
+    }
+
+    private JScrollPane getJScrollPane()
+    {
+        if(areaScrollPane == null)
+        {
+            areaScrollPane = new JScrollPane(getTextArea());
+        }
+        return areaScrollPane;
+    }
+
+    class OpenButtonEventHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            //open file dialog
+        }
+    }
+
+    class SaveButtonEventHandler implements ActionListener
+    {
+        public void actionPerformed(ActionEvent e)
+        {
+            //open file dialog
+        }
+    }
+
+
+    class ExitMenuItemHandler implements ActionListener{
+        public void actionPerformed(ActionEvent e){
+            getJFrame().dispose();
+        }
+    }
+
 
     public void run() {
         // runs the UI
 
         getJFrame().setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // todo: on mac change to close window
         getJFrame().setSize(600, 400);
-        getJFrame().setVisible(true);
         getJFrame().setJMenuBar(getMenuBar());
+
+        getJFrame().add(getJScrollPane());
+
+        getJFrame().setVisible(true);
     }
+
 }
